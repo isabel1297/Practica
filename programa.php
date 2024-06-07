65bc405b9a07e74765e2a1f0507b9d1d89d1d558
Erika-Arcos <earcosgomez863@gmail.com>
2024-06-07 07:11:09 -0600

    Agregar funcionalidad de división en programa.php

--- programa.php
+++ programa.php
@@ -4,10 +4,27 @@ echo "Erika Isabel Arcos Gomez";
 ?>
 
 <?php
-$num1 = 3;
+// Definir los números
+$num1 = 10;
 $num2 = 5;
+
+// Sumar los números
 $suma = $num1 + $num2;
-echo "Hola Mundo. La suma de $num1 y $num2 es $suma.";
+
+// Restar los números
+$resta = $num1 - $num2;
+
+// Multiplicar los números
+$multiplicacion = $num1 * $num2;
+
+// Dividir los números
+$division = $num1 / $num2;
+
+// Mostrar los resultados
+echo "Hola Mundo. La suma de $num1 y $num2 es $suma. <br>";
+echo "La resta de $num1 y $num2 es $resta. <br>";
+echo "La multiplicación de $num1 y $num2 es $multiplicacion. <br>";
+echo "La división de $num1 y $num2 es $division.";
 ?>
 
     
